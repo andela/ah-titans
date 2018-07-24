7646feb8d8d3b523e1f603a367b64e96f5a24cfd
ronaldndirangu <ronald.ndirangu@gmail.com>
2018-07-24 23:11:04 +0300

    Add descriptive validation errors

--- authors/apps/authentication/serializers.py
+++ authors/apps/authentication/serializers.py
@@ -4,6 +4,8 @@ from rest_framework import serializers
 
 from .models import User
 
+import re
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
     """Serializers registration requests and creates a new user."""
@@ -15,7 +17,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
         min_length=8,
         write_only=True
     )
-
     # The client should not be able to send a token along with a registration
     # request. Making `token` read-only handles that for us.
 
@@ -24,6 +25,41 @@ class RegistrationSerializer(serializers.ModelSerializer):
         # List all of the fields that could possibly be included in a request
         # or response, including fields specified explicitly above.
         fields = ['email', 'username', 'password']
+
+    def validate(self, data):
+        # The `validate` method is used to validate the email and password
+        # provided by the user during registration
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        # Validate the password has more than 8 characters
+        if not re.match(r"^(?=.{8,128}$).*", password):
+            raise serializers.ValidationError(
+                ' A password must be atleast 8 characters.'
+            )
+        # Validate password has at least one small and capital letter
+        elif not re.match(r"^(?=.*[A-Z])(?=.*[a-z]).*", password):
+            raise serializers.ValidationError(
+                ' A password must contain atleast one small letter and one capital letter.'
+            )
+        # Validate the password has atleast one number
+        elif not re.match(r"^(?=.*[0-9]).*", password):
+            raise serializers.ValidationError(
+                ' A password must contain atleast one number'
+            )
+
+        # Validate the email
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            raise serializers.ValidationError(
+                "Please enter a valid email"
+            )
+
+        if User.objects.filter(email=email):
+            raise serializers.ValidationError(
+                'A user with the same email already exists'
+            )
+
+        return data
 
     def create(self, validated_data):
         # Use the `create_user` method we wrote earlier to create a new user.
@@ -34,7 +70,6 @@ class LoginSerializer(serializers.Serializer):
     email = serializers.CharField(max_length=255)
     username = serializers.CharField(max_length=255, read_only=True)
     password = serializers.CharField(max_length=128, write_only=True)
-
 
     def validate(self, data):
         # The `validate` method is where we make sure that the current
@@ -94,7 +129,7 @@ class LoginSerializer(serializers.Serializer):
 class UserSerializer(serializers.ModelSerializer):
     """Handles serialization and deserialization of User objects."""
 
-    # Passwords must be at least 8 characters, but no more than 128 
+    # Passwords must be at least 8 characters, but no more than 128
     # characters. These values are the default provided by Django. We could
     # change them, but that would create extra work while introducing no real
     # benefit, so let's just stick with the defaults.
@@ -112,10 +147,9 @@ class UserSerializer(serializers.ModelSerializer):
         # specifying the field with `read_only=True` like we did for password
         # above. The reason we want to use `read_only_fields` here is because
         # we don't need to specify anything else about the field. For the
-        # password field, we needed to specify the `min_length` and 
+        # password field, we needed to specify the `min_length` and
         # `max_length` properties too, but that isn't the case for the token
         # field.
-
 
     def update(self, instance, validated_data):
         """Performs an update on a User."""
