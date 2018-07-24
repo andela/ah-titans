c15e4939e80f842ab89c76cd12a0609e77858a03
petermwash <petermwash48@gmail.com>
2018-07-24 15:44:15 +0300

    [FIX #159053947] Make corrections on tests

--- authors/tests/test_auth.py
+++ authors/tests/test_auth.py
@@ -2,46 +2,53 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from authors.apps.authentication.models import User
+from ..apps.authentication.models import User
 
 class ViewTestCase(TestCase):
     """Test suite for the api views."""
 
     def setUp(self):
         """Define the test client and other test variables."""
-
+        
         self.user_data = {
-            "username":"nerd",
-            "email":"nerd@nerd.com",
-            "password":"secret"
+            "user":{
+                "username": "nerd",
+                "email": "nerd@nerd.com",
+                "password": "secret"
+                }
+            }
+
+        self.login_data = {
+            "user":{
+                "email": "nerd@nerd.com",
+                "password": "secret"
+                }
             }
 
         # Initialize client
         self.client = APIClient()
 
     def test_api_can_create_a_user(self):
-        """Test the api can succefully create a user."""
+        """Test the api can successfully create a user."""
 
         response = self.client.post(
             '/api/users/',
-            self.user_data, format='json'
+            self.user_data,
+            format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_api_can_login_a_user(self):
-        """Test the api can succefuly login a user."""
+        """Test the api can successfuly login a user."""
 
-        login_data = {
-            "email":"nerd@nerd.com",
-            "password":"secret"
-            }
         self.client.post(
             '/api/users/',
-            self.user_data
+            self.user_data,
+            format='json'
         )
         response = self.client.post(
             '/api/users/login',
-            login_data,
+            self.login_data,
             format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -50,34 +57,41 @@ class ViewTestCase(TestCase):
         """Test the api cannot login a user with a wrong password."""
 
         login_data = {
-            "email":"nerd@nerd.com",
-            "password":"wrong"
+            "user":{
+                "email":"nerd@nerd.com",
+                "password":"wrong"
+            }
             }
         self.client.post(
             '/api/users/',
-            self.user_data
-        )
+            self.user_data,
+            format='json'
+        )   
         response = self.client.post(
             '/api/users/login',
             login_data,
             format='json'
         )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_api_login_with_wrong_email(self):
         """Test the api cannot login a user with a wrong email."""
 
         login_data = {
-            "email":"wrong@nerd.com",
-            "password":"secret"
+            "user":{
+                "email":"wrong@nerd.com",
+                "password":"secret"
             }
+            }
+
         self.client.post(
             '/api/users/',
-            self.user_data
+            self.user_data,
+            format='json'
         )
         response = self.client.post(
             '/api/users/login',
             login_data,
             format='json'
         )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- authors/tests/test_models.py
+++ authors/tests/test_models.py
@@ -2,20 +2,20 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from authors.apps.authentication.models import User
+from ..apps.authentication.models import User
 
 class ModelTestCase(TestCase):
     """This class defines the test suite for the article model."""
 
     def setUp(self):
         """Define the test client and other test variables."""
-        self.uzer = User.objects.create(username="nerd", email="nerd@nerd.com")
+        self.user = User.objects.create(username="nerd", email="nerd@nerd.com")
 
     def test_model_can_create_a_user(self):
         """Test the user model can create a user."""
-        self.assertTrue(isinstance(self.uzer, User))
+        self.assertTrue(isinstance(self.user, User))
 
     def test_model_returns_readable_representation(self):
         """Test a readable string is returned for the model instance."""
-        self.assertEqual(str(self.uzer), "nerd@nerd.com")
+        self.assertEqual(str(self.user), "nerd@nerd.com")
     
