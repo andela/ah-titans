a2e4f733f82293f8b68c8b91afe575d84fcce100
ronaldndirangu <ronald.ndirangu@gmail.com>
2018-07-26 19:02:22 +0300

    [Feature #159053961] Add create article feature

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import Article
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from .serializers import ArticleSeriaizer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class ArticleView(APIView):
+    lookup_field = 'slug'
+    query_set = Article.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = ArticleSeriaizer
+
+    def post(self, request):
+        article = request.data.get('article', {})
+        serializer = self.serializer_class(data=article)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
