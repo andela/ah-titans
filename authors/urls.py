d7088d6659cef515e09a7797d7b56b8d587c16ff
petermwash <petermwash48@gmail.com>
2018-07-26 18:49:04 +0300

    [Fix #159053960] Fix merge conflict

--- authors/urls.py
+++ authors/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import include,path
+from django.urls import include,path, url
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
 
-    url(r'^api/', include('authors.apps.authentication.urls', namespace='authentication')),
-    url(r'^api/', include('authors.apps.profiles.urls', namespace='profiles')),
+    path(r'^api/', include('authors.apps.authentication.urls', namespace='authentication')),
+    path(r'^api/', include('authors.apps.profiles.urls', namespace='profiles')),
 
 ]
