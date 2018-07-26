2574f5cde4ff171ebd8d6a1f40729fddcde67f23
John Musiu <johnmusiu@gmail.com>
2018-07-26 16:27:37 +0300

    [Chore #159053961] Create article model and model tests

--- authors/apps/articles/models.py
+++ authors/apps/articles/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import URLValidator
+from authors.apps.core.models import TimestampModel
+from authors.apps.authentication.models import User
 
-# Create your models here.
+class Article(TimestampModel):
+    """
+    Defines the articles table and functionality
+    """
+    title = models.CharField(db_index=True, max_length=255)
+    slug = models.SlugField(db_index=True, max_length=255, unique=True)
+    body = models.TextField()
+    description = models.TextField()
+    # ::TODO, implement authors once the profile feature is merged
+    # author = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class Image():
+    """
+    Defines images table which stores image URLS
+    Has a `belongs to one` relationship with the Article class
+    """
+    article = models.ForeignKey(Article, related_name='images', on_delete=models.CASCADE)
+    image_url = models.TextField(validators=[URLValidator()])
