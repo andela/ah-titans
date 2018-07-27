656cae594a2ef24d47f05abb521dcc11521a211e
John Musiu <johnmusiu@gmail.com>
2018-07-27 06:32:28 +0300

    [Chore #159053961] Refactors slug generation, adds docstrings

--- authors/apps/articles/models.py
+++ authors/apps/articles/models.py
@@ -1,11 +1,12 @@
+"""
+Module contains Models for article related tables
+"""
+from authors.apps.authentication.models import User
+from authors.apps.core.models import TimestampModel
 from django.db import models
 from django.core.validators import URLValidator
-from authors.apps.core.models import TimestampModel
-from authors.apps.authentication.models import User
 from django.db.models.signals import pre_save
-
 from django.utils.text import slugify
-
 
 class Article(TimestampModel):
     """
@@ -35,14 +36,21 @@ class Image():
 
 
 def pre_save_article_receiver(sender, instance, *args, **kwargs):
+    """
+    Method uses a signal to add slug to an article before saving it
+    A slug will always be unique
+    """
     slug = slugify(instance.title)
 
     #check if slug exists
     num = 1
-    while Article.objects.filter(slug=slug).exists():
-        slug = "%s-%s" %(slug, num)
+    unique_slug = slug
+    # loops until a unique slug is generated
+    while Article.objects.filter(slug=unique_slug).exists():
+        unique_slug = "%s-%s" %(slug, num)
         num += 1
 
-    instance.slug=slug
+    instance.slug = unique_slug
 
+# called just before a save is made in the db
 pre_save.connect(pre_save_article_receiver, sender=Article)
