1f6e9ddb2f4c94569a23d998c5a43bf8d6e1544a
petermwash <petermwash48@gmail.com>
2018-07-27 10:31:17 +0300

    [Fix 159053960] Fix profile url error

--- authors/urls.py
+++ authors/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import include,path
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
 
 
-    path(r'^api/', include('authors.apps.authentication.urls', namespace='authentication')),
-    path(r'^api/', include('authors.apps.profiles.urls', namespace='profiles')),
+    url(r'^api/', include('authors.apps.authentication.urls', namespace='authentication')),
+    url(r'^api/', include('authors.apps.profiles.urls', namespace='profiles')),
 
 ]
