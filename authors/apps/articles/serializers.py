f4afd4884412f46b1bbcbeeff2bb7ad001c3718a
John Musiu <johnmusiu@gmail.com>
2018-07-31 18:53:31 +0300

    [Feature #159053961] Refactor tests and styling

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -25,19 +25,20 @@ class ArticleSerializer(serializers.ModelSerializer):
         return Article.objects.create(**validated_data)
 
     def validate(self, data):
-        # The `validate` method is used to validate the title, description and body
+        # The `validate` method is used to validate the title,
+        # description and body
         # provided by the user during creating or updating an article
         title = data.get('title', None)
         description = data.get('description', None)
 
         # Validate title is not a series of symbols or non-alphanumeric characters
-        if re.match(r"[!@#$%^&*~\{\}()]{2,}", title):
+        if re.match(r"[!@#$%^&*~\{\}()][!@#$%^&*~\{\}()]{2,}", title):
             raise serializers.ValidationError(
                 """A title must not contain two symbols/foreign characters following each other"""
             )
         # Validate the description is not a series of symbols or
         # non-alphanumeric characters
-        if re.match(r"[!@#$%^&*~\{\}()]{2,}", description):
+        if re.match(r"[!@#$%^&*~\{\}()][!@#$%^&*~\{\}()]{2,}", description):
             raise serializers.ValidationError(
                 """A description must not contain two symbols/foreign characters following each other"""
             )
