17bcf4ff84fefb5c5fb7537ae61ea8d3aab421e0
chepmanu <emmanuelchepsire33@gmail.com>
2018-08-02 22:58:35 +0300

    [Chore #159053959] Updated some tests

--- authors/tests/test_social_auth.py
+++ authors/tests/test_social_auth.py
@@ -12,33 +12,6 @@ class ViewTestCase(TestCase):
 
 class Testsocial(ViewTestCase, APITestCase):
     """This class defines the test suite all social auth."""
-    
-    def test_google(self):
-        """
-        Test login by google
-        """
-        access_token = os.environ.get("GoogleAccessToken")
-        response = self.client.post('/api/users/auth/google-oauth2',
-            data={'access_token':access_token})
-
-      
-
-        self.assertContains(response, "token")
-
-        self.assertEqual(response.status_code, 200)
-
-
-    def test_facebook(self):
-        """
-        Test login by facebook
-        """
-        access_token = os.environ.get("FbAccessToken", "none")
-        response = self.client.post('/api/users/auth/facebook',
-            data={'access_token':access_token})
-
-        self.assertContains(response, "token")
-
-        self.assertEqual(response.status_code, 200)
 
 
     def test_only_allowed_backends_work(self):
