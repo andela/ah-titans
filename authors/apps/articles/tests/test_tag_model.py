01a4a75037986404af79b48e46344476bd6f3425
petermwash <petermwash48@gmail.com>
2018-08-08 08:26:45 +0300

    [Chore #159053967] Refactor some tests

--- authors/apps/articles/tests/test_tag_model.py
+++ authors/apps/articles/tests/test_tag_model.py
@@ -1,37 +1,7 @@
 from django.test import TestCase
 from rest_framework import status
 
-from authors.apps.authentication.models import User
-from authors.apps.articles.models import Article, Tag
-
-class CreateArticle():
-    def __init__(self):
-        self.title = "Django is life"
-        self.body = """Try it today. Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim 
-                ad minim veniam, quis nostrud exercitation ullamco laboris"""
-        self.description = "Django"
-        self.tagList = ["Django-rest", "Django-taggit"]
-
-    def create_a_user(self, username="nerd", email="nerd@nerd.com", password="Secret123456"):
-        """Creating a test user"""
-        user = User.objects.create_user(username, email, password)
-        user.save()
-        return user
-
-    def create_article(self):
-        """Creating a test article with a taglist"""
-        user = self.create_a_user()
-        article = models.Article.objects.create(
-            title=self.title,
-            description=self.description,
-            body=self.body,
-            tagList=self.tagList,
-            author=user.profile)
-        article.save()
-        return article
-
+from authors.apps.articles.models import Tag
 
 class ModelTestCase(TestCase):
     """
